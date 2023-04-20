549d972f66a90169f4e98420e24e2a285df7001f
Stanislav Bezzhanov <sbezzhanov@yandex.ru>
2023-04-20 22:21:19 +0300

    initial commit

--- Source.cpp
+++ Source.cpp
@@ -2,9 +2,6 @@
 #include <fstream>
 
 class Adress {
-
-
-
 public:
 	Adress() {};
 	Adress(std::string City, std::string Street, int House, int Apparts) {
@@ -14,10 +11,25 @@ public:
 		this->Apparts = Apparts;
 	};
 
-	void get_output_address(std::ofstream(&outtxt)) {
+	void get_output_address(int count, std::ofstream(&outtxt)) {
 
 		outtxt << this->City << ", " << this->Street << ", " << this->House << ", " << this->Apparts << std::endl;
 
+	};
+
+	void sort(Adress* adress_arr, int count) {
+		
+		for (int i = 0; i < count; ++i) {
+			for(int j = i + 1; j < count; ++j) {
+				if(adress_arr[i].City > adress_arr[j].City) {
+			Adress temp_adr = adress_arr[i];
+			adress_arr[i] = adress_arr[j];
+			adress_arr[j] = temp_adr;
+
+				}
+			}
+	}
+		
 	};
 
 protected:
@@ -47,23 +59,25 @@ int main() {
 			intxt >> House;
 			intxt >> Apparts;
 			adress_arr[i] = Adress(City, Street, House, Apparts);
-
-			std::ofstream(outtxt);
-			outtxt.open("out.txt");
-
-			if (outtxt) {
-				for (int i = 0; i < count; ++i) {
-					adress_arr[i].get_output_address(outtxt);
-
-				}
-				outtxt.close();
-			}
-			else {
-				std::cout << "output file not found!" << std::endl;
-			}
-
+			
 		}
+		//
+		adress_arr->sort(adress_arr, count);
+		std::ofstream(outtxt);
+		outtxt.open("out.txt");
+		if (outtxt) {
+			outtxt << count << std::endl;
+			for (int i = 0; i < count; ++i) {
+				adress_arr[i].get_output_address(count, outtxt);
 
+			}
+			outtxt.close();
+		}
+		else {
+			std::cout << "output file not found!" << std::endl;
+		}
+		delete[] adress_arr;
+		adress_arr = nullptr;
 	}
 	else {
 		std::cout << "input file not found!" << std::endl;
